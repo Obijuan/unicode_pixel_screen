754aa4cc1627b065afa4ffcf4362410b8d15006c
obijuan <obijuan.cube@gmail.com>
2025-09-14 10:07:51 +0200

    add example

---
+++ wiki/Log/Examples/Ej-04-pant-1x1-anim-2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# ──────────────────────────────────────────────────────
+# ── Pantalla 1x1. Animaciones sencillas
+# ──────────────────────────────────────────────────────
+import curses
+import time
+
+# ────── Tiempo de espera entre fotogramas
+WAIT = 0.3
+
+
+# ────────────────────────────────────────────────────────────
+# ── Generar la animacion a partir de la lista de caracteres
+# ────────────────────────────────────────────────────────────
+def animate(anim):
+
+    # ── Numero de veces a repetir la animacion
+    for _ in range(4):
+
+        # ── Animación de los caracteres dados
+        for car in anim:
+
+            # ── Mostrar caracter actual y esperar
+            stdscr.addstr(0, 0, car)
+            stdscr.refresh()
+            time.sleep(WAIT)
+
+
+# ────────────────
+# ──     MAIN
+# ────────────────
+
+# ────── Caracteres de la animación
+anim1 = ['⠁', '⠂', '⠄', '⡀']
+anim2 = ['⠈', '⠐', '⠠', '⢀']
+
+# ────── Constantes para determinar la visibilidad del cursor
+ON = 2
+OFF = 0
+
+# ────── Inicializacion
+stdscr = curses.initscr()
+
+# ── Ocultar el cursor
+curses.curs_set(OFF)
+
+animate(anim1)
+animate(anim2)
+
+stdscr.addstr(2, 0, "Pulsa una tecla")
+
+# ── Esperar a que el usuario apriete una tecla
+stdscr.getch()
+
+# ── Volver a enseñar el cursor antes de terminar
+curses.curs_set(ON)
+
+# ── Terminar. Volver a la pantalla original del terminal
+curses.endwin()
