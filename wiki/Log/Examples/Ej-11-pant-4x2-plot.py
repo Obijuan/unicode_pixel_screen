d430fafca39875c67e932c600cbac7c36488f36e
obijuan <obijuan.cube@gmail.com>
2025-09-15 21:00:50 +0200

    add example

---
+++ wiki/Log/Examples/Ej-11-pant-4x2-plot.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# ──────────────────────────────────────────────────────
+# ── Pantalla 4x2. Funcion de plot persistente
+# ──────────────────────────────────────────────────────
+import curses
+import time
+
+# ────── Tiempo de espera entre fotogramas
+WAIT = 0.1
+
+# ────── Pantalla virtual de 4x2 pixeles
+pantalla = [
+    [0, 0, 0, 0],   # ── y = 0
+    [0, 0, 0, 0],   # ── y = 1
+]
+
+
+# ────── Refrescar la pantalla 4x1 en la pantalla de ncurses
+def pantalla_refresh():
+    for y in range(2):
+        for x in range(4):
+            stdscr.addstr(y, x, chr(0x2800 + pantalla[y][x]))
+
+    stdscr.refresh()
+
+
+# ──────────────────────────────────────────────────────────────────────
+# ── Obtener el codigo unicode braile (byte) a partir de la posicion
+# ── del pixel (x,y) dentro del carácter
+# ── x ∈ [0,7]
+# ── y ∈ [0,7]
+# ──────────────────────────────────────────────────────────────────────
+def braile_from_pos(x, y):
+
+    # ──── La funcion tiene valores diferentes segun los casos
+    # ──── Caso normal (y<3)
+    # ──── Caso especial (y=3)
+    cod = 1 << (3*x + y) if y < 3 else 1 << (x + 6)
+    return cod
+
+
+# ──────────────────────────────────────────────────────────────────────
+# ── Dibujar en la pantalla 4x2 el pixel de la posicion (x,y)
+# ── x ∈ [0,7]
+# ── y ∈ [0,7]
+# ── El ploteado se hace en la pantalla virtual, y se combina con
+# ── lo que ya hubiese antes
+# ──────────────────────────────────────────────────────────────────────
+def plot(x, y):
+
+    # ── Obtener las coordenadas del carácter
+    xc = x >> 1
+    yc = y >> 2
+
+    # ── Obtener las coordenadas locales
+    xl = x & 0x01
+    yl = y & 0x03
+
+    # ── Obtener el byte a partir de (xl, yl)
+    b = braile_from_pos(xl, yl)
+
+    # ── Obtener el valor actual de la pantalla
+    value = pantalla[yc][xc]
+
+    # ── Calcular el nuevo valor
+    value = value | b
+
+    # ── Actualizar la pantalla virtual
+    pantalla[yc][xc] = value
+
+    # ── Mostrarlo en la pantalla real
+    pantalla_refresh()
+
+
+# ────────────────
+# ──     MAIN
+# ────────────────
+
+# ────── Constantes para determinar la visibilidad del cursor
+ON = 2
+OFF = 0
+
+# ────── Inicializacion
+stdscr = curses.initscr()
+
+# ── Ocultar el cursor
+curses.curs_set(OFF)
+
+stdscr.addstr(2, 0, "0123")
+stdscr.addstr(0, 4, "0")
+stdscr.addstr(1, 4, "1")
+# ───────── Dibujar todos los pixeles
+
+# ── Para cada linea...
+for y in range(8):
+
+    # ── Dibujar pixeles en la linea actual
+    for x in range(8):
+        plot(x, y)
+        time.sleep(WAIT)
+
+
+stdscr.addstr(3, 0, "Pulsa una tecla")
+
+# ── Esperar a que el usuario apriete una tecla
+stdscr.getch()
+
+# ── Volver a enseñar el cursor antes de terminar
+curses.curs_set(ON)
+
+# ── Terminar. Volver a la pantalla original del terminal
+curses.endwin()
